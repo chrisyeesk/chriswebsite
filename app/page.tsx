28f7285697cf22c7a66c7c2d371a10b39a81d38f
chrisyeesk <165258742+chrisyeesk@users.noreply.github.com>
2024-10-04 18:54:07 +1000

    font load

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Chatbot from './components/ChatBot';
 import { Contact } from './components/Contact';
 import { Education } from './components/Education';
 import Experiences from './components/Experiences';
+import Head from 'next/head';
 import Footer from './components/Footer';
 import { Projects } from './components/Projects';
 import TechStack from './components/TechStack';
@@ -14,23 +15,31 @@ import localFont from '@next/font/local';
 // const roboto = localFont({ src: './components/font/roboto.woff2' });
 const manrope = localFont({
   src: './components/font/Manrope-ExtraLight.woff2',
+  display: 'swap',
 });
 
 export default function Home() {
   return (
-    <main
+    <><Head>
+      <link
+        rel="preload"
+        href="/components/font/Manrope-ExtraLight.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous" />
+    </Head><main
       className={`${manrope.className} mx-auto justify-center flex text-GeistSans min-h-screen w-full flex-col`}
     >
-      <Introduction />
-      <TestimonyCards />
-      <TechStack />
-      <Experiences />
-      <Projects />
-      <Certificates />
-      <Education />
-      <Contact />
-      <Chatbot/>
-      <Footer />
-    </main>
+        <Introduction />
+        <TestimonyCards />
+        <TechStack />
+        <Experiences />
+        <Projects />
+        <Certificates />
+        <Education />
+        <Contact />
+        <Chatbot />
+        <Footer />
+      </main></>
   );
 }
