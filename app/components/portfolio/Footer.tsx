facab2107cb30c93e9bb981f4b616b0b2d0f2146
chrisyeesk <165258742+chrisyeesk@users.noreply.github.com>
2025-10-08 04:50:09 +1000

    removed unwanted files

--- app/components/portfolio/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <div className="2xl:justify-center 2xl:text-xl 2xl:mb-16 px-4 mb-5 text-sm sm:text-md flex xl:justify-start justify-center sm:px-20 md:px-4 lg:px-40">
-      <div>Copyright © Chris Portfolio.</div>
-    </div>
-  );
-}
